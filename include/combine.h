189e0b83d12517e12a5653c8a6139892ea974a91
Anz <knecht_roger@hotmail.com>
2011-04-23 23:56:51 +0200

    added combine.h header

---
+++ include/combine.h
@@ -0,0 +1,13 @@
+#ifndef COMBINE_H
+#define COMBINE_H
+
+#include "array.h"
+#include "camera.h"
+#include "combine.h"
+#include "map.h"
+#include "render.h"
+#include "scene.h"
+#include "sprite.h"
+#include "texture.h"
+
+#endif
